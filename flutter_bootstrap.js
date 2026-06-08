8e0e745cffb1855317029946f48ccd48528cdc6d
devyasserbm <s444006391@uqu.edu.sa>
2026-06-08 03:13:40 +0300

    Redeploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "664903085"
+    serviceWorkerVersion: "1332023303"
   }
 });
